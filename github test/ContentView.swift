aeeefd4548e6d45173e865b62e8d5acd332b5df5
Amol Srivastava <amolsrivastavamoney@gmail.com>
2021-04-08 20:44:18 -0500

    Initial Commit

---
+++ github test/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  github test
+//
+//  Created by Amol Srivastava on 4/8/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ github test/github_testApp.swift
@@ -0,0 +1,17 @@
+//
+//  github_testApp.swift
+//  github test
+//
+//  Created by Amol Srivastava on 4/8/21.
+//
+
+import SwiftUI
+
+@main
+struct github_testApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
