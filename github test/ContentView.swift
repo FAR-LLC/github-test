1ae40df2e024097f973893e6b6dd5cf8ff91e9a4
Rayhaan <rayhaan994@gmail.com>
2021-04-08 20:54:37 -0500

    1.4

--- github test/ContentView.swift
+++ github test/ContentView.swift
@@ -9,7 +9,7 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
+        Text("Hello, amol!")
             .padding()
             .background(Color .black)
 
